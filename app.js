49225acb6e73b559c2a1237d7b241b4479723fbc
Lucas-OF28 <luuu_of@hotmail.com>
2022-08-02 16:32:40 -0300

    routes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const db = require('./db/connection')
+const bodyParser =  require('body-parser')
 
 const PORT = 3000
 
 app.listen(PORT, function(){
   console.log(`Rodando na porta ${PORT}`)
 })
+
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}))
 
 //DB CONNECTION
 db
@@ -22,3 +27,6 @@ db
 app.get('/', (req,res) => {
   res.send("Funcionando")
 })
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'))
