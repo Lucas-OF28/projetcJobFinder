714862bca91ed26c30a9697bc87d808a8f3e11ae
Lucas-OF28 <luuu_of@hotmail.com>
2022-08-03 15:42:16 -0300

    handlebars, e filtro de busca

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
+const path = require('path')
 const db = require('./db/connection')
 const bodyParser =  require('body-parser')
+const Job = require('./models/Job')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 const PORT = 3000
 
@@ -12,6 +17,15 @@ app.listen(PORT, function(){
 
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}))
+
+//handle bars
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //DB CONNECTION
 db
@@ -25,8 +39,37 @@ db
 
 // ROUTES
 app.get('/', (req,res) => {
-  res.send("Funcionando")
+
+  let search = req.query.job
+  let query = '%'+search+'%' // PH -> PHP, Word -> Wordpress
+
+  if(!search){
+    Job.findAll({order: [
+     ['createdAt','DESC']
+    ]})
+    .then(jobs =>{
+
+    res.render('index', {
+      jobs
+    })
+
+  })
+  .catch(err => console.log(err))
+} else{
+    Job.findAll({
+      where:{title: {[Op.like]: query}},
+      order: [
+    ['createdAt','DESC']
+  ]})
+  .then(jobs =>{
+    res.render('index', {
+      jobs, search
+    })
+  }).catch(err => console.log(err))
+
+  }
 })
+
 
 //jobs routes
 app.use('/jobs', require('./routes/jobs'))
